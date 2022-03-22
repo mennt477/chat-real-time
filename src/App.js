150a06ac9104e593af34be64f2c4e1b1c5e31819
mennt477 <98246233+mennt477@users.noreply.github.com>
2022-03-22 15:31:14 +0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,1 +1,17 @@
+// import React from 'react';
+import './App.css';
+import Login from './components/Login';
+import ChatRoom from './components/ChatRoom';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
+
+function App() {
+  return  <Router>
+            <Switch>
+              <Route path="/login" component={Login} />
+              <Route path="/" component={ChatRoom} />
+            </Switch>
+        </Router>
+}
+
+export default App;
