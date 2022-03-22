da163e8822c228834bf436c36286de5ca563cb5c
mennt477 <98246233+mennt477@users.noreply.github.com>
2022-03-22 17:21:35 +0700

    Update AuthProvider.js

--- src/Context/AuthProvider.js
+++ src/Context/AuthProvider.js
@@ -1,31 +1,30 @@
- import React, { useEffect, useState } from 'react'
+ import React, { useState } from 'react'
 import { useHistory } from 'react-router-dom';
 import { auth } from '../firebase/config';
 import {Spin} from 'antd';
 
 export const AuthContext = React.createContext();
 
-export default function AuthProvider({children}){
+export default function AuthProvider({ children }){
   const history = useHistory();
   const [user,setUser] = useState({});
   const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
-    const unsubcribed = auth.onAuthStateChanged((user) => {
+  React.useEffect(() => {
+    const unsubscibed = auth.onAuthStateChanged((user) => {
       console.log({user})
       if(user){
-        const { displayName, email, uid, photoUrl } = user;
+        const { displayName, email, uid, photoURL } = user;
         setUser({
-          displayName, email, uid, photoUrl
+          displayName, email, uid, photoURL
         })
         setIsLoading(false);
         history.push('/');
-      }
-      history.push('/login');
-    })
+      }else history.push('/login');
+    });
     //clean func
     return () => {
-      unsubcribed()
+      unsubscibed()
     }
   }, [history])
 
